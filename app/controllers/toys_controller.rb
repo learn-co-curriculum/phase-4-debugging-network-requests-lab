c8d0fea8e823774e1477b997a51b56ed29dc7da3
L. Cadet <85368055+Ludcad615@users.noreply.github.com>
2022-07-11 17:06:34 -0400

    commit tree b8a8b47868cd010cdfa22e3499979dc19021670a

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,13 +7,14 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy
   end
 
   def destroy
